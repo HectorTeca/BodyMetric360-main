45d472ef93930b3cb13a926092567bee32360ac6
HectorTeca <hectorroberto.teca@alumnos.ulagos.cl>
2023-12-04 22:30:31 -0300

    *Agregado postActivity.kt: Activity para el formulario principal

---
+++ app/src/main/java/com/ulagos/myapplication/tmb/postActivity.kt
@@ -0,0 +1,12 @@
+package com.ulagos.myapplication.tmb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ulagos.myapplication.R
+
+class postActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
